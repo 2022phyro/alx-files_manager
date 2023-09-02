4e1f1daf0b63942f9787add3a9647e240a898f34
2022phyro <phyrokelstein2@gmail.com>
2023-09-02 11:16:42 -0700

    Fixed eslint errors

--- utils/db.js
+++ utils/db.js
@@ -1,19 +1,22 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient } from 'mongodb';
 
 class DBClient {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || '27017';
     const database = process.env.DB_DATABASE || 'files_manager';
-    this.cli = new MongoClient(`mongodb://${host}:${port}/${database}`, { useUnifiedTopology: true } );
+    this.cli = new MongoClient(`mongodb://${host}:${port}/${database}`, { useUnifiedTopology: true });
     this.cli.connect();
   }
+
   isAlive() {
     return this.cli.isConnected();
   }
+
   async nbUsers() {
     return this.cli.db().collection('users').countDocuments();
   }
+
   async nbFiles() {
     return this.cli.db().collection('files').countDocuments();
   }
