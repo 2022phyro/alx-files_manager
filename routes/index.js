a4fafc05b47299add8cc6886fecd27357cc045f4
2022phyro <phyrokelstein2@gmail.com>
2023-09-05 08:34:01 -0700

    Fixed the error in the routes

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,8 @@ export default function Routes(app) {
   app.get('/disconnect', middleFromToken, AuthController.getDisconnect);
   app.get('/connect', middleFromAuth, AuthController.getConnect);
   app.post('/files', middleFromToken, FilesController.postUpload);
-  app.get('/files:id', middleFromToken, FilesController.getShow);
-  app.get('/files:id/publish', middleFromToken, FilesController.putPublish);
-  app.get('/files:id/publish', middleFromToken, FilesController.putUnpublish);
+  app.get('/files/:id', middleFromToken, FilesController.getShow);
+  app.get('/files/:id/publish', middleFromToken, FilesController.putPublish);
+  app.get('/files/:id/unpublish', middleFromToken, FilesController.putUnpublish);
   app.get('/files', middleFromToken, FilesController.getIndex);
 }
